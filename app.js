39f2e9ec97b544d7ff9f8008674e2b21cbbb0e82
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-10-12 01:09:14 +0900

    Setting ExpressJS and Git

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Server started");
+const express = require("express");
+const app = express();
+
+// MonGoDB connect
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+//1 Entry codes
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2 Session codes
+
+//3 View codes
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 Routing codes - BSSR
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://user01:X6fRByv7C1oEjwXb@nodejs.i14q1bn.mongodb.net/todolist_test?retryWrites=true&w=majority"; // database name: todolist_test
+// 3)
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) console.log(`Error on connecting to MongoDB`);
+    else {
+      console.log("MongoDB connection succeeded");
+
+      module.exports = client;
+
+      const app = require("./app");
+
+      const server = http.createServer(app);
+      let PORT = 3003;
+      server.listen(PORT, function () {
+        console.log(
+          `server is running on port ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
