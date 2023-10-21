45e90ce8e2f136375661d28beb6f8303149d0622
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-10-21 19:57:08 +0900

    feat: develop Product controller logic

--- router_bssr.js
+++ router_bssr.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/productController");
 
 /********************************
  *            BSSR EJS         *
@@ -16,6 +17,15 @@ router_bssr
   .post("/login", restaurantController.loginProcess);
 
 router_bssr.get("/logout", restaurantController.logout);
+router_bssr.get("/check-me", restaurantController.checkSessions);
+
+router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.post(
+  "/products/create",
+  restaurantController.validateAuthRestaurant,
+  productController.addNewProduct
+);
+router_bssr.post("/products/edit/:id", productController.updateChoosenProduct);
 
 // other routers (methods)
 
