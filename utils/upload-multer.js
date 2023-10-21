e8dd4a9cfe47e9b7306cc46758cc7b72ee9327cd
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-10-21 21:43:56 +0900

    feat: multer single image upload process fix

--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -2,17 +2,24 @@ const path = require("path");
 const multer = require("multer");
 const uuid = require("uuid");
 
-const product_storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/products");
-  },
-  filename: function (req, file, cb) {
-    console.log(file);
-    const extension = path.parse(file.originalname).ext;
-    const random_name = uuid.v4() + extension;
+/** MULTER IMAGE UPLOADER */
+function getTargetImageStorage(address) {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./uploads/${address}`);
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      const extension = path.parse(file.originalname).ext;
+      const random_name = uuid.v4() + extension;
+      cb(null, random_name);
+    },
+  });
+}
 
-    cb(null, random_name);
-  },
-});
+const makeUploader = (address) => {
+  const storage = getTargetImageStorage(address);
+  return multer({ storage: storage });
+};
 
-module.exports.uploadProductImage = multer({ storage: product_storage });
+module.exports = makeUploader;
