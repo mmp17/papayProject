6faaf3a740d88eff8b26c56a866edb7dfad112c7
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-10-26 11:05:36 +0900

    comments

--- router.js
+++ router.js
@@ -1,13 +1,14 @@
+// This module is a definition of various routes using the Express Router.
 const express = require("express");
-const router = express.Router();
-const memberController = require("./controllers/memberController");
+const router = express.Router(); //  An instance of the Express router to define and manage routes.
+const memberController = require("./controllers/memberController"); // This is an imported module that contains methods to handle the logic for member-related endpoints, such as signup, login, and logout.
 
 /********************************
  *      React REST API          *
  * ******************************/
 
 // member related routers
-router.post("/signup", memberController.signup);
+router.post("/signup", memberController.signup); // Handles the signup process. Calls the signup method (cb fn) from memberController.
 router.post("/login", memberController.login);
 router.get("/logout", memberController.logout);
 
